e323bceaff439fe06b903300e3c4929836fd29ba
Nhật Anh <lenhatanh86@gmail.com>
2018-04-11 11:08:00 +0700

    Init

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let btn = document.getElementById("apply")
+let grids = document.getElementById("grids")
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  let total = document.getElementById("total").value
+  grids.innerHTML = "";
+  for (let i = 1; i <= total; i++) {
+    grids.innerHTML += "<div class='grid'>" + i + "</div>"
+  }
+})
+
+grids.addEventListener("click", (e) => {
+  e.preventDefault()
+  let elm = e.target
+  if (elm.classList.contains('grid')) {
+    elm.classList.toggle('active')
+  }
+})
